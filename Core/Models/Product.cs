9871a92d09ff17fcfb4c50532b98cb399ec03f91
Sulav <dah19001@byui.edu>
2021-07-28 13:46:43 -0600

    Initial Setup E-commerce  Doremifa

---
+++ Core/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace Core.Models
+{
+    public class Product
+    {
+        // Primary Key 
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+        
+    }
+}
